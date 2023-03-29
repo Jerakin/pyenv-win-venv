bdedca4abce80b8d80f8caba407c4a247f236d6c
Jerakin <the_rensvind@hotmail.com>
2023-03-29 18:34:03 +0200

    fix: something made it not work

--- bin/pyenv-venv-install.bat
+++ bin/pyenv-venv-install.bat
@@ -1,11 +1,8 @@
 @echo off
 IF EXIST %USERPROFILE%\.pyenv\pyenv-win\versions\%1 (
 	IF NOT EXIST %USERPROFILE%/.pyenv-venv/versions/%2 (
-	
-		:: Activate the pyenv 
 		pyenv shell %1
 		
-		:: Figure out if we are post 3.3 with where env became a standard lib
 		FOR /F %%I IN ('python -c "import platform;print(platform.python_version())"') DO @SET "PYVERSION_FULL=%%I"
 		FOR /F "tokens=1,2 delims=." %%i IN ("%PYVERSION_FULL%") DO (
 			SET M_VERSION=%%i
@@ -14,18 +11,19 @@ IF EXIST %USERPROFILE%\.pyenv\pyenv-win\versions\%1 (
 		SET POST_33=1
 
 		IF %M_VERSION% EQU 3 (
-			IF %P_VERSION% LEQ 3 (
+			IF %P_VERSION% LSS 3 (
 				SET POST_33=0
 			)
 		)
+		
 		IF %M_VERSION% EQU 2 (
 			SET POST_33=0
 		)
-
+		
 		IF %POST_33% EQU 1 (
 			python -m venv %USERPROFILE%/.pyenv-venv/versions/%2
 		) ELSE (
-			pip install virtualenv 
+			pip install virtualenv
 			python -m virtualenv "%USERPROFILE%/.pyenv-venv/versions/%2"
 		)
 
@@ -35,6 +33,3 @@ IF EXIST %USERPROFILE%\.pyenv\pyenv-win\versions\%1 (
 )ELSE (
 	echo Python v%1 is not installed. Install using 'pyenv install %1'
 )
-
-
-

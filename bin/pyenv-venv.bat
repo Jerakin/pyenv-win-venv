995bca27ba32a17e8d530c6e74485a3aa7171c65
Jerakin <the_rensvind@hotmail.com>
2023-03-29 20:31:12 +0200

    help if command doesn't exist

--- bin/pyenv-venv.bat
+++ bin/pyenv-venv.bat
@@ -1,26 +1,35 @@
 @echo off 
+set IS_COMMAND=0
 IF "%1" == "install" (
+	set IS_COMMAND=1
 	call %~dp0pyenv-venv-install %2 %3
 )
 IF "%1" == "uninstall" (
+	set IS_COMMAND=1
 	call %~dp0pyenv-venv-uninstall %2
 )
 IF "%1" == "activate" (
+	set IS_COMMAND=1
 	call %~dp0pyenv-venv-activate %2
 )
 IF "%1" == "deactivate" (
+	set IS_COMMAND=1
 	call %~dp0pyenv-venv-deactivate
 )
 IF "%1" == "list" (
+	set IS_COMMAND=1
 	call %~dp0pyenv-venv-list
 )
 IF "%1" == "help" (
+	set IS_COMMAND=1
 	call %~dp0pyenv-venv-help
 )
 IF "%1" == "which" (
+	set IS_COMMAND=1
 	call %~dp0pyenv-venv-which
 )
 IF "%1" == "" (
+	set IS_COMMAND=1
 	call %~dp0pyenv-venv-help
 )
 IF %IS_COMMAND% EQU 0 (

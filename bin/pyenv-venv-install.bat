5021dd662cb8ea201828c09065139167a2d49f2a
Jerakin <the_rensvind@hotmail.com>
2023-03-29 20:00:55 +0200

    fix: flattened installation logic

--- bin/pyenv-venv-install.bat
+++ bin/pyenv-venv-install.bat
@@ -1,35 +1,33 @@
 @echo off
-IF EXIST %USERPROFILE%\.pyenv\pyenv-win\versions\%1 (
-	IF NOT EXIST %USERPROFILE%/.pyenv-venv/versions/%2 (
-		pyenv shell %1
-		
-		FOR /F %%I IN ('python -c "import platform;print(platform.python_version())"') DO @SET "PYVERSION_FULL=%%I"
-		FOR /F "tokens=1,2 delims=." %%i IN ("%PYVERSION_FULL%") DO (
-			SET M_VERSION=%%i
-			SET P_VERSION=%%j
-		)
-		SET POST_33=1
 
-		IF %M_VERSION% EQU 3 (
-			IF %P_VERSION% LSS 3 (
-				SET POST_33=0
-			)
-		)
-		
-		IF %M_VERSION% EQU 2 (
-			SET POST_33=0
-		)
-		
-		IF %POST_33% EQU 1 (
-			python -m venv %USERPROFILE%/.pyenv-venv/versions/%2
-		) ELSE (
-			pip install virtualenv
-			python -m virtualenv "%USERPROFILE%/.pyenv-venv/versions/%2"
-		)
-
-	) ELSE (
-		echo '%2' already exists. Please choose another name for the env.
-	)
-)ELSE (
+IF NOT EXIST %USERPROFILE%\.pyenv\pyenv-win\versions\%1 (
 	echo Python v%1 is not installed. Install using 'pyenv install %1'
+	goto :eof
 )
+IF EXIST %USERPROFILE%/.pyenv-venv/versions/%2 (
+	echo '%2' already exists. Please choose another name for the env.
+	goto :eof
+)
+
+cmd /C pyenv shell %1
+
+FOR /F %%I IN ('python -c "import platform;print(platform.python_version())"') DO @SET "PYVERSION_FULL=%%I"
+FOR /F "tokens=1,2 delims=." %%i IN ("%PYVERSION_FULL%") DO (
+	SET /a M_VERSION=%%i
+	SET /a P_VERSION=%%j
+)
+
+IF %M_VERSION% EQU 2 goto :USE_VIRTUALENV
+
+IF %M_VERSION% EQU 3 IF %P_VERSION% LSS 3 goto :USE_VIRTUALENV
+
+goto :USE_VENV
+
+:USE_VIRTUALENV
+python -W ignore:DEPRECATION -m pip --disable-pip-version-check install --quiet virtualenv
+python -m virtualenv --quiet "%USERPROFILE%/.pyenv-venv/versions/%2"
+goto :eof
+
+:USE_VENV
+python -m venv %USERPROFILE%/.pyenv-venv/versions/%2
+goto :eof

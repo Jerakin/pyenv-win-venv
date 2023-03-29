6b62caf593ebb395a26c91c4e3fa69cef9c2e3d4
Jerakin <the_rensvind@hotmail.com>
2023-03-29 21:50:44 +0200

    doc: readme updates

--- README.md
+++ README.md
@@ -22,10 +22,20 @@ Download the repository and add the `/bin` folder to your PATH variable.
     A CLI to manage virtual envs with pyenv-win
 
     Commands:
+        init                searches for a .python-version file in the 
+                            current directory and activates the env
+        local               set the active or provided env name in the
+                            current directory
         activate            activate an env
         deactivate          deactivate an env
         install             install an env
         uninstall           uninstall an env
         list                list all installed environments versions
+        which <command>     shows the path to an executable
         help                show this information
 ```
+
+
+# Automatic activation using `.python-version`
+**This is NOT possible**  
+While that is possible in the unix version [pyenv-virtualenv](https://github.com/pyenv/pyenv-virtualenv) as well as the powershell version [pyenv-win-venv](https://github.com/pyenv-win/pyenv-win-venv) cmd.exe does not have a life time file such as `profile.ps1`.
--- bin/pyenv-venv-help.bat
+++ bin/pyenv-venv-help.bat
@@ -1,18 +1,22 @@
 @echo off
 IF "%1" == "" (
-	echo Copyright ^(c^) Mattias Hedberg ^<the_rensind@hotmail.com^>
-	echo. 
-	echo Usage: pyenv-venv ^<command^> ^<args^>
-	echo. 
-	echo A CLI to manage virtual envs with pyenv-win
-	echo. 
-	echo Commands:
-	echo     activate            activate an env
-	echo     deactivate          deactivate an env
-	echo     install             install an env
-	echo     uninstall           uninstall an env
-	echo     list                list all installed environments versions
-	echo     which <command>     shows the path to an executable
-	echo     help                show this information
+    echo Copyright ^(c^) Mattias Hedberg ^<the_rensind@hotmail.com^>
+    echo. 
+    echo Usage: pyenv-venv ^<command^> ^<args^>
+    echo. 
+    echo A CLI to manage virtual envs with pyenv-win
+    echo. 
+    echo Commands:
+    echo     init                searches for a .python-version file in the 
+    echo                         current directory and activates the env
+    echo     local               set the active or provided env name in the
+    echo                         current directory
+    echo     activate            activate an env
+    echo     deactivate          deactivate an env
+    echo     install             install an env
+    echo     uninstall           uninstall an env
+    echo     list                list all installed environments versions
+    echo     which <command>     shows the path to an executable
+    echo     help                show this information
 
 )
